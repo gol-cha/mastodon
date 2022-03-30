534ae1d16f41fefa92e7ebf775a8e99a42d5f6c9
John Doe <info@mevo.xyz>
2022-03-31 08:34:07 +0900

    Remove gzip compression by Rails backend

--- config/application.rb
+++ config/application.rb
@@ -165,7 +165,6 @@ module Mastodon
     config.active_job.queue_adapter = :sidekiq
 
     config.middleware.use Rack::Attack
-    config.middleware.use Rack::Deflater
 
     config.to_prepare do
       Doorkeeper::AuthorizationsController.layout 'modal'
