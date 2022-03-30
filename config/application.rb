626ee87c708e15c93ade0fde8e34fb8933d0a7ee
John Doe <info@mevo.xyz>
2022-03-31 08:31:28 +0900

    Revert "Support brotli compression on Rails"
    This reverts commit dddecd79dc30a499d847e255f2148ce28e661806.

--- config/application.rb
+++ config/application.rb
@@ -166,7 +166,6 @@ module Mastodon
 
     config.middleware.use Rack::Attack
     config.middleware.use Rack::Deflater
-    config.middleware.use Rack::Brotli
 
     config.to_prepare do
       Doorkeeper::AuthorizationsController.layout 'modal'
